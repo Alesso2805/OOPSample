3c022299480a680814db961d5790ff0b63a6cb3c
Alessandro David <alessandrodp2222@gmail.com>
2024-09-09 16:27:49 -0500

    feat: Inserted Program.cs values and tested code

--- OOPSample/Program.cs
+++ OOPSample/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OOPSample.Console.Sales.Domain.Model.Aggregates;
+
+//Crear una nueva orden de venta
+
+        var SalesOrder = new SalesOrder(1, 1001);    // id  = 1, customerId = 1001
+
+        // Agregar items a la orden
+        SalesOrder.AddItem(2001, 2, 25.76); // 31 productId = 2001, quantity = 2, unitPrice = 15.50
+        SalesOrder.AddItem(2002, 1, 25.99); // 25 productId = 2002, quantity = 1, unitPrice = 25.00
+        SalesOrder.AddItem(2003, 5, 18.67); // 30 productId = 2003, quantity = 3, unitPrice = 10.00
+
+        //Calcular el precio total de la orden
+        double totalPrice = SalesOrder.CalculateTotalPrice();
+
+        // Mostrar el precio total en la consola
+        Console.WriteLine($"El precio total de la orden es {totalPrice:C}");
