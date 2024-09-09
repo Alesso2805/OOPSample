9311d9c4f7fc2e76a83a395a309bb2d713198321
Alessandro Alonso David Polanco <u202122232@upc.edu.pe>
2024-09-09 12:26:06 -0500

    ¨feat: implement SalesOrder and SalesOrderItem.

--- OOPSample.Console/Sales/Domain/Model/Aggregates/SalesOrder.cs
+++ OOPSample.Console/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -1,6 +1,11 @@
 ﻿namespace OOPSample.Console.Sales.Domain.Model.Aggregates;
 
-public class SalesOrder
+public class SalesOrder(int id)
 {
-    
+    private List<SalesOrderItem> _items = [];
+    private int Id { get; } = id;
+    public void AddItem(int productId, int quantity)
+    {
+        _items.Add(new SalesOrderItem(Id, productId, quantity));
+    }
 }
--- OOPSample.Console/Sales/Domain/Model/Aggregates/SalesOrderItem.cs
+++ OOPSample.Console/Sales/Domain/Model/Aggregates/SalesOrderItem.cs
@@ -1,6 +1,9 @@
 ﻿namespace OOPSample.Console.Sales.Domain.Model.Aggregates;
-
-public class SalesOrderItem
+public class SalesOrderItem(int salesOrderId, int productId, int
+    quantity)
 {
-    
+    public int Id { get; set; } = 0;
+    public int SalesOrderId { get; set; } = salesOrderId;
+    public int ProductId { get; set; } = productId;
+    public int Quantity { get; set; } = quantity;
 }
